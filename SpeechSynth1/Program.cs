d55242acbdca5edb930d9883ddce063802b60974
capetonian <andrew.lagrange@strategicthinking.co.za>
2013-08-14 21:48:04 +0200

    Speech Recog + Generate

---
+++ SpeechSynth1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeechSynth1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			SpeechSynthesizer speaker = new SpeechSynthesizer();
+			speaker.SetOutputToDefaultAudioDevice();
+			string speak = "Welcome";
+
+			while (speak != "quit")
+			{
+				speaker.Speak(speak);
+				speak = Console.ReadLine();
+			}
+
+		}
+	}
+}
